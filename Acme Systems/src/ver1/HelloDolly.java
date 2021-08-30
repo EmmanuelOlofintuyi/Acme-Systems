3c974d1c466893ba6f006d8c01cf2ad299e4e538
Emmanuel Olofintuyi <EmmanuelOlofintuyi@gmail.com>
2021-08-30 08:01:13 -0400

    added messag to Hello Dolly

--- Acme Systems/src/ver1/HelloDolly.java
+++ Acme Systems/src/ver1/HelloDolly.java
@@ -3,5 +3,6 @@ package ver1;
 public class HelloDolly {
 	public static void main(String[] args) {
 		System.out.println("Please give me an A Dr. Gibson, lol");
+		System.out.println("Hello Dolly from Eclipse Again");
     }
 }
